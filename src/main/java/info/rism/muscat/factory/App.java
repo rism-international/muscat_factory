537b2a28f31b81e47e90fb196f3724ee3a6d2f8b
Stephan Hirsch <stephan.hirsch@gmail.com>
2019-10-25 12:57:40 +0200

    Export all models

--- src/main/java/info/rism/muscat/factory/App.java
+++ src/main/java/info/rism/muscat/factory/App.java
@@ -1,6 +1,8 @@
 package info.rism.muscat.factory;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  * Main class
@@ -10,10 +12,21 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-    	MarcConfig marcConfig = new MarcConfig("muscat/config/marc/tag_config_work.yml");
-    	FieldContent fieldContent = new FieldContent("work.txt");
-        MarcxmlBuilder marcxmlBuilder = new MarcxmlBuilder("output/work.xml");
-        
-        marcxmlBuilder.build(marcConfig, fieldContent, "00000cam a2200000 a 4500");
+    	String dirString = System.getProperty("user.dir");
+    	File folder = new File(dirString + "/muscat/config/marc/");
+    	File[] listOfFiles = folder.listFiles();
+    	ArrayList<String> models = new ArrayList<String>();
+    	for (File file : listOfFiles) {
+    		String filenameString = file.getName();
+    		if (filenameString.indexOf("tag_config_") >= 0) {
+    			models.add(file.getName().replace("tag_config_", "").replace(".yml", ""));
+    		}    		
+    	}
+    	for (String model : models) {
+    		MarcConfig marcConfig = new MarcConfig("muscat/config/marc/tag_config_" + model + ".yml");
+    		FieldContent fieldContent = new FieldContent(model + ".txt");
+    		MarcxmlBuilder marcxmlBuilder = new MarcxmlBuilder("output/"+ model + ".xml");        
+    		marcxmlBuilder.build(marcConfig, fieldContent, "00000cam a2200000 a 4500");
+    	}
     }
 }
